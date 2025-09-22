5b4e554aafefbba7bdb94bc2dec91ed996cce4aa
AndersonKeller <andersonkellervendas@hotmail.com>
2025-09-22 19:16:17 -0300

    add logout

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function getUser(){
     email.innerText = emailLocal
 }
 getUser()
+
+function logout(){
+    const buttonLogout = document.querySelector("#logout")
+    console.log(buttonLogout) 
+    buttonLogout.addEventListener("click",()=>{
+        console.log("loogout")
+        localStorage.clear()
+           const email = document.querySelector("#email")
+           email.innerText = ""
+    })
+}
+logout()
